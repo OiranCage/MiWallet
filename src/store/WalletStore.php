0c46d43b4409e07071e64cca7630a222d27e2504
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-25 21:04:40 +0900

    xuidをキーにしてWalletを保存するStoreを実装

---
+++ src/store/WalletStore.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace oiran\store;
+
+
+use oiran\model\Wallet;
+
+class WalletStore
+{
+	public static array $walletMap = [];
+
+	public static function add(Wallet $wallet) {
+		self::$walletMap[$wallet->getOwnerXuid()] = $wallet;
+	}
+
+	public static function delete(string $xuid) {
+		unset(self::$walletMap[$xuid]);
+	}
+
+	public static function findBy(string $xuid): ?Wallet {
+		return self::$walletMap[$xuid] ?? null;
+	}
+}
