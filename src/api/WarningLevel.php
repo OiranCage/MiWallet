41b517531f90682aa64ceba596277ad08ff0d5ca
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-25 18:54:39 +0900

    警告レベルの定数をもつクラスを作成

---
+++ src/api/WarningLevel.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace oiran;
+
+
+class WarningLevel
+{
+	// It will not alert you when the Wallet::moneyAmount changes.
+	public const NONE = 0;
+
+	// Throws an exception if the value of Wallet::moneyAmount is invalid.
+	public const THROW_EXCEPTION = 1;
+
+	// If the value of Wallet::moneyAmount is invalid, the method will return false.
+	public const RETURN_FAILED_RESULT = 2;
+}
