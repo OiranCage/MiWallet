675873b393d19339d5162d6c038d803206e9f026
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-25 19:01:05 +0900

    初期化メソッドを作成

--- src/api/WalletLib.php
+++ src/api/WalletLib.php
@@ -5,12 +5,29 @@ namespace oiran;
 
 
 use Exception;
+use oiran\model\Option;
+use oiran\storage\OptionStorage;
 
 class WalletLib
 {
 	private static ?WalletLib $instance = null;
 
-	public static function init() {
+	/**
+	 * @param string $dataFileName
+	 * @param string $dataFilePath
+	 * @param int $warningLevel
+	 */
+	public static function init(
+		string $dataFileName,
+		string $dataFilePath,
+		int $warningLevel = WarningLevel::RETURN_FAILED_RESULT
+	) {
+		OptionStorage::init(new Option($warningLevel, $dataFileName));
+
+		$dataFileName .= ".json";
+		if(!file_exists($fullPath = $dataFilePath.$dataFileName)) {
+			file_put_contents($fullPath, "{}");
+		}
 	}
 
 	/**
@@ -20,4 +37,6 @@ class WalletLib
 	public static function getInstance(): WalletLib {
 		return self::$instance ?? throw new Exception("library not initialized.");
 	}
+
+	private function __construct() {}
 }
