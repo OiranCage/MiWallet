f197bfba5af7618029127b30130a5ef645e58929
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-26 20:02:50 +0900

    decodeする際にファイルの中身を渡していなかった部分を修正

--- src/usecase/OutputWalletDataUseCase.php
+++ src/usecase/OutputWalletDataUseCase.php
@@ -10,6 +10,6 @@ class OutputWalletDataUseCase
 {
 	public static function execute(): array {
 		$option = OptionStorage::getOption();
-		return json_decode($option->getFullPath(), true);
+		return json_decode(file_get_contents($option->getFullPath()), true);
 	}
 }
