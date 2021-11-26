02c350fe9cd6c3c7470433ffae53607a661d0d28
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-26 15:17:00 +0900

    Singletonからオブジェクトを持つパターンに変更

--- src/api/WalletLib.php
+++ src/api/WalletLib.php
@@ -7,24 +7,27 @@ namespace oiran\walletlib\api;
 use Exception;
 use oiran\walletlib\model\Option;
 use oiran\walletlib\storage\OptionStorage;
+use oiran\walletlib\store\WalletStore;
 
 class WalletLib
 {
-	private static ?WalletLib $instance = null;
+	private static ?WalletStore $store = null;
 
 	public static function init(string $fileName, string $folderPath, int $warningLevel = WarningLevel::DO_NOT_PROCESS) {
 		OptionStorage::init($option = new Option($warningLevel, $fileName.".json", $folderPath));
 		if(!file_exists($option->getFullPath())) {
 			file_put_contents($option->getFullPath(), "{}");
 		}
+
+		self::$store = new WalletStore();
 	}
 
 	/**
-	 * @return WalletLib
+	 * @return WalletStore
 	 * @throws Exception
 	 */
-	public static function getInstance(): WalletLib {
-		return self::$instance ?? throw new Exception("library not initialized.");
+	public static function store(): WalletStore {
+		return self::$store ?? throw new Exception("Store not initialized.");
 	}
 
 	private function __construct() {}
