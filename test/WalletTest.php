9b2ddad2b62926bdd0c9246a16c45ee559d3ccf2
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-26 19:19:54 +0900

    テスト追加

--- test/WalletTest.php
+++ test/WalletTest.php
@@ -5,6 +5,7 @@ namespace test;
 
 
 use oiran\walletlib\api\WalletLib;
+use oiran\walletlib\api\WarningLevel;
 use oiran\walletlib\model\Wallet;
 
 class WalletTest
@@ -24,5 +25,32 @@ class WalletTest
 		WalletLib::save();
 
 		var_dump(WalletLib::repository()->getDecodeWalletMap());
+
+
+		$wallet = new Wallet("test", "test", 0);
+		$result = $wallet->earnCoin(PHP_INT_MAX + 1);
+		echo "earn result: $result";
+
+		$wallet = new Wallet("test", "test", 0);
+		$result = $wallet->spendCoin(100);
+		echo "earn result: $result";
+
+
+		WalletLib::init("test", "C:\Users\admin\Desktop\\", WarningLevel::NONE);
+		echo "Init Success.".PHP_EOL;
+
+		$wallet = new Wallet("test", "test", 0);
+		$wallet->earnCoin(PHP_INT_MAX + 1);
+		echo $wallet->getMoney();
+
+		$wallet = new Wallet("test", "test", 0);
+		$wallet->spendCoin(100);
+		echo $wallet->getMoney();
+
+
+		WalletLib::init("test", "C:\Users\admin\Desktop\\", WarningLevel::THROW_EXCEPTION);
+		echo "Init Success.".PHP_EOL;
+
+		$wallet->earnCoin(PHP_INT_MAX + 1);
 	}
 }
