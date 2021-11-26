8282278937d95e4425f85888fe6b50ca62019243
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-26 16:13:14 +0900

    Walletのencode/decodeをするDTOクラスを実装

---
+++ src/dto/WalletDTO.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace oiran\walletlib\dto;
+
+
+use oiran\walletlib\model\Wallet;
+
+class WalletDTO
+{
+	public static function decode(string $xuid, array $data): Wallet {
+		return new Wallet($xuid, $data["ownerName"], $data["moneyAmount"]);
+	}
+
+	public static function encode(Wallet $wallet): array {
+		return [
+			$wallet->getOwnerXuid() => [
+				"moneyAmount" => $wallet->getMoney(),
+				"ownerName" => $wallet->getOwnerName()
+			]
+		];
+	}
+}
