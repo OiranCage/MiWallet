b9f111025be19f1ba55bd31fc712f2ddc01a4f86
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-28 17:11:54 +0900

    プラグインのライフサイクルクラスを追加

---
+++ src/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace oiran\walletlib;
+
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+	protected function onEnable(): void {
+		// TODO; Init
+	}
+
+	protected function onDisable(): void {
+		// TODO; save cached Wallet.
+	}
+}
