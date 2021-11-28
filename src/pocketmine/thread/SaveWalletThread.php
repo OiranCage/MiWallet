ac31625447375451e5cfc1699284afda0065d589
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-28 17:29:21 +0900

    Walletを永久化するスレッドを追加

---
+++ src/pocketmine/thread/SaveWalletThread.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace oiran\walletlib\pocketmine\thread;
+
+
+use pocketmine\scheduler\AsyncTask;
+
+class SaveWalletThread extends AsyncTask
+{
+	public function onRun(): void {
+		// TODO: Implement onRun() method.
+	}
+}
