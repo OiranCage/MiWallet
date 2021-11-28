b57328576911b40327ac9abc7cf237d4915fc8ed
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-28 18:19:17 +0900

    非同期書き込みを実装

--- src/pocketmine/thread/SaveWalletThread.php
+++ src/pocketmine/thread/SaveWalletThread.php
@@ -8,7 +8,21 @@ use pocketmine\scheduler\AsyncTask;
 
 class SaveWalletThread extends AsyncTask
 {
+	private bool $login = false;
+
+	public function __construct(
+		private string $path,
+		private string $jsonData,
+		private string $xuid
+	) {}
+
 	public function onRun(): void {
-		// TODO: Implement onRun() method.
+		file_put_contents($this->path, $this->jsonData);
+	}
+
+	public function onCompletion(): void {
+		if($this->login) {
+			// TODO; add Wallet to Store
+		}
 	}
 }
