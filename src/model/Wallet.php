909f7d8bb69d603772451d6a70a412acfbba3f8f
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-28 17:28:34 +0900

    OptionStorageからOptionPoolにリファクタリング

--- src/model/Wallet.php
+++ src/model/Wallet.php
@@ -6,7 +6,7 @@ namespace oiran\walletlib\model;
 
 use Exception;
 use oiran\walletlib\api\WarningLevel;
-use oiran\walletlib\storage\OptionStorage;
+use oiran\walletlib\pool\OptionPool;
 
 class Wallet
 {
@@ -19,7 +19,7 @@ class Wallet
 	public function earnCoin(int $value): bool {
 		$result = $this->moneyAmount + $value;
 		if(PHP_INT_MAX < $result) {
-			switch (OptionStorage::getOption()->getWarningLevel()) {
+			switch (OptionPool::getOption()->getWarningLevel()) {
 				case WarningLevel::THROW_EXCEPTION:
 					throw new Exception("Invalid money amount earned ($value).");
 				case WarningLevel::DO_NOT_PROCESS:
@@ -34,7 +34,7 @@ class Wallet
 	public function spendCoin(int $value): bool {
 		$result = $this->moneyAmount - $value;
 		if($this->moneyAmount < 0) {
-			switch (OptionStorage::getOption()->getWarningLevel()) {
+			switch (OptionPool::getOption()->getWarningLevel()) {
 				case WarningLevel::THROW_EXCEPTION:
 					throw new Exception("Invalid money amount spent ($value).");
 				case WarningLevel::DO_NOT_PROCESS:
