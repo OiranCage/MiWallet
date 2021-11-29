b7b0bbda4b8283d76cf37e10c8f5691f59bad258
ribnil <40230941+ribnil@users.noreply.github.com>
2021-11-29 17:10:14 +0900

    Moneyを実装

---
+++ src/model/Money.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace oiran\walletlib\model;
+
+
+class Money
+{
+	public function __construct(
+		private int $amount
+	) {}
+
+	public function add(Money $money): Money {
+		if(PHP_INT_MAX - $this->amount < $money->getAmount()) {
+			return new Money(PHP_INT_MAX);
+		} else {
+			return new Money($this->amount + $money->getAmount());
+		}
+	}
+
+	public function sub(Money $money): Money {
+		if($this->amount - $money->getAmount() < 0) {
+			return new Money(0);
+		} else {
+			return new Money($money->getAmount() - $this->amount);
+		}
+	}
+
+	public function getAmount(): int {
+		return $this->amount;
+	}
+}
