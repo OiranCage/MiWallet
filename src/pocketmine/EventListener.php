7427e7f83ed967404f24a0fa04ef95fc057e483c
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-01 20:09:21 +0900

    PlayerがJoin/Quit時の実装

--- src/pocketmine/EventListener.php
+++ src/pocketmine/EventListener.php
@@ -4,6 +4,12 @@
 namespace oiran\walletlib\pocketmine;
 
 
+use oiran\walletlib\api\WalletLib;
+use oiran\walletlib\dto\WalletDTO;
+use oiran\walletlib\model\Wallet;
+use oiran\walletlib\pool\OptionPool;
+use oiran\walletlib\store\WalletStore;
+use oiran\walletlib\utility\FolderPath;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
@@ -11,10 +17,18 @@ use pocketmine\event\player\PlayerQuitEvent;
 class EventListener implements Listener
 {
 	public function onPlayerJoinEvent(PlayerJoinEvent $event) {
-		// TODO; add to Player Wallet to Store
+		$name = $event->getPlayer()->getName();
+		$xuid = $event->getPlayer()->getXuid();
+		$folder = OptionPool::getOption()->get("wallet_data_folder");
+		$filePath = FolderPath::get().$folder.$xuid.".json";
+
+		$wallet = file_exists($filePath)
+			? WalletDTO::decode($xuid, json_decode(file_get_contents($filePath)))
+			: new Wallet($xuid, $name, OptionPool::getOption()->get("default_money"));
+		WalletStore::add($wallet);
 	}
 
 	public function onPlayerQuitEvent(PlayerQuitEvent $event) {
-		// TODO; save Player Wallet of Store
+		WalletLib::save(WalletStore::findBy($event->getPlayer()->getXuid()));
 	}
 }
