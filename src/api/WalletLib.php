1388896dd77099b8b8b18e6bab6c10416fafcd1b
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-01 20:23:44 +0900

    データに変更があった場合に永久化するように

--- src/api/WalletLib.php
+++ src/api/WalletLib.php
@@ -8,14 +8,21 @@ use oiran\walletlib\model\Wallet;
 use oiran\walletlib\pocketmine\thread\SaveWalletThread;
 use oiran\walletlib\pool\OptionPool;
 use oiran\walletlib\pool\ThreadPool;
+use oiran\walletlib\store\WalletStore;
 use oiran\walletlib\utility\FolderPath;
 
 class WalletLib
 {
 	public static function save(Wallet $wallet) {
-		$folder = OptionPool::getOption()->get("wallet_data_folder");
-		$filePath = FolderPath::get().$folder.$wallet->getOwnerXuid().".json";
-		ThreadPool::submit(new SaveWalletThread($wallet, $filePath));
+		if ($wallet->isChanged()) {
+			$folder = OptionPool::getOption()->get("wallet_data_folder");
+			$filePath = FolderPath::get().$folder.$wallet->getOwnerXuid().".json";
+			ThreadPool::submit(new SaveWalletThread($wallet, $filePath));
+		}
+	}
+
+	public static function findWallet(string $xuid): ?Wallet {
+		return WalletStore::findBy($xuid);
 	}
 
 	private function __construct() {}
