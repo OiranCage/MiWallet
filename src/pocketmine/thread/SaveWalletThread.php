1e32fecce5cee6b75484c28935aeaeb6157fa640
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-01 19:58:03 +0900

    永久化の実装をリファクタリング

--- src/pocketmine/thread/SaveWalletThread.php
+++ src/pocketmine/thread/SaveWalletThread.php
@@ -4,14 +4,19 @@
 namespace oiran\walletlib\pocketmine\thread;
 
 
+use oiran\walletlib\dto\WalletDTO;
+use oiran\walletlib\model\Wallet;
 use pocketmine\scheduler\AsyncTask;
 
 class SaveWalletThread extends AsyncTask
 {
-	public function __construct(
-		private string $path,
-		private string $jsonData
-	) {}
+	private string $xuid;
+	private string $jsonData;
+
+	public function __construct(Wallet $wallet, private string $path) {
+		$this->xuid = $wallet->getOwnerXuid();
+		$this->jsonData = WalletDTO::encode($wallet);
+	}
 
 	public function onRun(): void {
 		file_put_contents($this->path, $this->jsonData);
