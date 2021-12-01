45438edfbb0c5de90f8c3d50586f521694c1d6d8
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-01 20:09:52 +0900

    データ永久化の関数をライブラリクラスに実装

--- src/api/WalletLib.php
+++ src/api/WalletLib.php
@@ -4,43 +4,18 @@
 namespace oiran\walletlib\api;
 
 
-use Exception;
-use oiran\walletlib\model\Option;
-use oiran\walletlib\repository\WalletRepository;
+use oiran\walletlib\model\Wallet;
+use oiran\walletlib\pocketmine\thread\SaveWalletThread;
 use oiran\walletlib\pool\OptionPool;
-use oiran\walletlib\store\WalletStore;
-use oiran\walletlib\usecase\InputWalletDataUseCase;
-use oiran\walletlib\usecase\OutputWalletDataUseCase;
+use oiran\walletlib\pool\ThreadPool;
+use oiran\walletlib\utility\FolderPath;
 
 class WalletLib
 {
-	private static ?WalletStore $store = null;
-	private static ?WalletRepository $repository = null;
-
-	public static function init(string $fileName, string $folderPath) {
-		OptionPool::init($option = new Option($fileName.".json", $folderPath));
-		if(!file_exists($option->getFullPath())) {
-			file_put_contents($option->getFullPath(), json_encode([]));
-		}
-
-		self::join();
-	}
-
-	public static function join() {
-		self::$store = new WalletStore();
-		self::$repository = new WalletRepository();
-	}
-
-	public static function save(int $jsonFlag = JSON_PRETTY_PRINT) {
-		InputWalletDataUseCase::execute($jsonFlag);
-	}
-
-	public static function store(): WalletStore {
-		return self::$store /*?? throw new Exception("WalletStore not initialized.")*/;
-	}
-
-	public static function repository(): WalletRepository {
-		return self::$repository /*?? throw new Exception("WalletRepository not initialized.")*/;
+	public static function save(Wallet $wallet) {
+		$folder = OptionPool::getOption()->get("wallet_data_folder");
+		$filePath = FolderPath::get().$folder.$wallet->getOwnerXuid().".json";
+		ThreadPool::submit(new SaveWalletThread($wallet, $filePath));
 	}
 
 	private function __construct() {}
