068812b9f680ebead94055d1aa2dccba96862587
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-01 19:06:46 +0900

    スレッドがデータを保存する役割だけを持つように

--- src/pocketmine/thread/SaveWalletThread.php
+++ src/pocketmine/thread/SaveWalletThread.php
@@ -8,21 +8,12 @@ use pocketmine\scheduler\AsyncTask;
 
 class SaveWalletThread extends AsyncTask
 {
-	private bool $login = false;
-
 	public function __construct(
 		private string $path,
-		private string $jsonData,
-		private string $xuid
+		private string $jsonData
 	) {}
 
 	public function onRun(): void {
 		file_put_contents($this->path, $this->jsonData);
-	}
-
-	public function onCompletion(): void {
-		if($this->login) {
-			// TODO; add Wallet to Store
-		}
 	}
 }
