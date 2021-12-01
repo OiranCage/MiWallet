e852da867af0b3271265394426a048c534e38c66
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-01 19:33:13 +0900

    ConfigのInitとWalletStoreのデータを永久化する部分を実装

--- src/Main.php
+++ src/Main.php
@@ -4,15 +4,33 @@
 namespace oiran\walletlib;
 
 
+use oiran\walletlib\pool\OptionPool;
+use oiran\walletlib\store\WalletStore;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase
 {
 	protected function onEnable(): void {
-		// TODO; Init
+		OptionPool::init(new Config($this->getDataFolder()."option.json", Config::JSON, [
+			"default_money" => 0,
+			"wallet_data_folder" => "player".DIRECTORY_SEPARATOR
+		]));
 	}
 
 	protected function onDisable(): void {
-		// TODO; save cached Wallet.
+		$walletDataFolder = OptionPool::getOption()->get("wallet_data_folder");
+		$filePathPrefix = $this->getDataFolder().$walletDataFolder;
+		foreach (WalletStore::getWalletMap() as $wallet) {
+			if ($wallet->isChanged()) {
+				file_put_contents(
+					$filePathPrefix.$wallet->getOwnerXuid().".json",
+					json_encode([
+						"ownerName" => $wallet->getOwnerName(),
+						"moneyAmount" => $wallet->getMoney()->getAmount()
+					])
+				);
+			}
+		}
 	}
 }
