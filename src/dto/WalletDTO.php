dc1aba9e8f897e60d692392871e06e186e66bacc
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-01 19:57:09 +0900

    encodeがjson形式の文字列を出力するように

--- src/dto/WalletDTO.php
+++ src/dto/WalletDTO.php
@@ -12,10 +12,10 @@ class WalletDTO
 		return new Wallet($xuid, $data["ownerName"], $data["moneyAmount"]);
 	}
 
-	public static function encode(Wallet $wallet): array {
-		return [
+	public static function encode(Wallet $wallet): string {
+		return json_encode([
 			"moneyAmount" => $wallet->getMoney(),
 			"ownerName" => $wallet->getOwnerName()
-		];
+		], JSON_PRETTY_PRINT);
 	}
 }
