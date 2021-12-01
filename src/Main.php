0b19125d34e95c7058ff1ef56e1c574a58d82c4d
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-01 19:41:42 +0900

    PoolのInitを実装

--- src/Main.php
+++ src/Main.php
@@ -5,17 +5,29 @@ namespace oiran\walletlib;
 
 
 use oiran\walletlib\pool\OptionPool;
+use oiran\walletlib\pool\ThreadPool;
 use oiran\walletlib\store\WalletStore;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\AsyncPool;
 use pocketmine\utils\Config;
 
 class Main extends PluginBase
 {
 	protected function onEnable(): void {
-		OptionPool::init(new Config($this->getDataFolder()."option.json", Config::JSON, [
+		OptionPool::init($option = new Config($this->getDataFolder()."option.json", Config::JSON, [
 			"default_money" => 0,
-			"wallet_data_folder" => "player".DIRECTORY_SEPARATOR
+			"wallet_data_folder" => "player".DIRECTORY_SEPARATOR,
+			"pool_size" => 2,
+			"worker_memory_limit" => 256
 		]));
+
+		ThreadPool::init(new AsyncPool(
+			$option->get("pool_size"),
+			$option->get("worker_memory_limit"),
+			$this->getServer()->getLoader(),
+			$this->getServer()->getLogger(),
+			$this->getServer()->getTickSleeper()
+		));
 	}
 
 	protected function onDisable(): void {
