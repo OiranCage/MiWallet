c47d01eadba5c372471ac201ad6ebdab813342c7
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-02 16:54:10 +0900

    データがストアにない場合にRepositoryからデータを参照するようにする

--- src/api/WalletLib.php
+++ src/api/WalletLib.php
@@ -8,13 +8,19 @@ use oiran\walletlib\model\Wallet;
 use oiran\walletlib\pocketmine\thread\SaveWalletThread;
 use oiran\walletlib\pool\OptionPool;
 use oiran\walletlib\pool\ThreadPool;
+use oiran\walletlib\repository\WalletRepository;
 use oiran\walletlib\store\WalletStore;
 use oiran\walletlib\utility\FolderPath;
 
 class WalletLib
 {
 	public static function findWallet(string $xuid): ?Wallet {
-		return WalletStore::findBy($xuid);
+		$wallet = WalletStore::findBy($xuid);
+		if ($wallet === null) {
+			$wallet = WalletRepository::findBy($xuid);
+		}
+
+		return $wallet;
 	}
 
 	private function __construct() {}
