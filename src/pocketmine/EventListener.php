561d7520997e35863c9394595969a19fa81e3f04
ribnil <40230941+ribnil@users.noreply.github.com>
2021-12-03 13:47:26 +0900

    非同期でデータを読み取る場合のロジックを追加

--- src/pocketmine/EventListener.php
+++ src/pocketmine/EventListener.php
@@ -4,8 +4,12 @@
 namespace oiran\walletlib\pocketmine;
 
 
+use oiran\walletlib\model\Money;
 use oiran\walletlib\model\Wallet;
+use oiran\walletlib\pocketmine\event\WalletCreateEvent;
+use oiran\walletlib\pocketmine\thread\CreateWalletThread;
 use oiran\walletlib\pool\OptionPool;
+use oiran\walletlib\pool\ThreadPool;
 use oiran\walletlib\repository\WalletRepository;
 use oiran\walletlib\store\WalletStore;
 use pocketmine\event\Listener;
@@ -15,18 +19,23 @@ use pocketmine\event\player\PlayerQuitEvent;
 class EventListener implements Listener
 {
 	public function onPlayerJoinEvent(PlayerJoinEvent $event) {
+		$name = $event->getPlayer()->getName();
 		$xuid = $event->getPlayer()->getXuid();
-		$wallet = WalletRepository::findBy($xuid);
-		if ($wallet === null) {
-			$wallet = new Wallet(
-				$xuid,
-				$event->getPlayer()->getName(),
-				OptionPool::getOption()->get("default_money"),
-				true
-			);
-		}
+		$defaultMoney = OptionPool::getOption()->get("default_money");
 
-		WalletStore::add($wallet);
+		if (OptionPool::getOption()->get("async_create_wallet")) {
+			ThreadPool::submit(new CreateWalletThread($xuid, $name, $defaultMoney));
+		} else {
+			$wallet = WalletRepository::findBy($xuid);
+			if ($wallet === null) {
+				$wallet = new Wallet($xuid, $name, new Money($defaultMoney), true);
+			}
+
+			WalletStore::add($wallet);
+
+			$event = new WalletCreateEvent($wallet);
+			$event->call();
+		}
 	}
 
 	public function onPlayerQuitEvent(PlayerQuitEvent $event) {
